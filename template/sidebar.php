c5ba46adffa26d554a00fb19f2b586def9c67fec
5ganes <kh6ganesh@gmail.com>
2016-06-28 21:45:56 +0545

    up-- to info officer

--- template/sidebar.php
+++ template/sidebar.php
@@ -1,17 +1,6 @@
 <div id="sidebar-left" class="sidebar cf">
 	<div id="widgets-wrap-sidebar-left">
-		<div id="text-2" class="widget-sidebar frontier-widget widget_text">
-			<?php
-			$info_officer=$groups->getById(INFO_OFFICER); $info_officer=$conn->fetchArray($info_officer);
-			?>
-			<h4 class="widget-title"><?php echo $info_officer['name'];?></h4>
-			<div class="textwidget">
-				<a href="<?=$info_officer['urlname']; ?>">
-				<img src="<?=CMS_GROUPS_DIR.$info_officer['image'];?>" <figcaption="">
-				<?=$info_officer['shortcontents']; ?>
-				</a>
-			</div>
-		</div>
+
 		<div id="text-9" class="widget-sidebar frontier-widget widget_text">
 	      <?php
 	      $officer=$groups->getById(OFFICER); $officer=$conn->fetchArray($officer);
@@ -23,6 +12,20 @@
 	      </div>
 	      <a></a>
 	    </div>
+
+	    <div id="text-2" class="widget-sidebar frontier-widget widget_text">
+			<?php
+			$info_officer=$groups->getById(INFO_OFFICER); $info_officer=$conn->fetchArray($info_officer);
+			?>
+			<h4 class="widget-title"><?php echo $info_officer['name'];?></h4>
+			<div class="textwidget">
+				<a href="<?=$info_officer['urlname']; ?>">
+				<img src="<?=CMS_GROUPS_DIR.$info_officer['image'];?>" <figcaption="">
+				<?=$info_officer['shortcontents']; ?>
+				</a>
+			</div>
+		</div>
+
 		<div id="text-3" class="widget-sidebar frontier-widget widget_text">
 			<h4 class="widget-title">उपयोगी लिङ्क्स</h4>
 			<div class="textwidget">

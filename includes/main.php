6682277412fefa66f0813ef488cd8947270654ee
5ganes <kh6ganesh@gmail.com>
2016-06-28 21:41:58 +0545

    msg from Chief and info officer

--- includes/main.php
+++ includes/main.php
@@ -50,23 +50,7 @@
 
 <div id="sidebar-right" class="sidebar cf">
   <div id="widgets-wrap-sidebar-right">
-    <div id="text-9" class="widget-sidebar frontier-widget widget_text">
-      <?php
-      $officer=$groups->getById(OFFICER); $officer=$conn->fetchArray($officer);
-      ?>
-      <h4 class="widget-title"><?=$officer['name'];?></h4>
-      <div class="textwidget">
-        <a><img src="<?=CMS_GROUPS_DIR.$officer['image'];?>"></a>
-        <?=$officer['shortcontents'];?>
-      </div>
-      <a></a>
-    </div>
     
-    <div id="text-6" class="widget-sidebar frontier-widget widget_text">
-      <div class="textwidget">
-        <a href="bills.php"><img src="images/Untitled.png"></a>
-      </div>
-    </div>
     
     <div id="ewic-widget-2" class="widget-sidebar frontier-widget widget_ewic_sc_widget">
       <div id="ewic-conwidget-75">
@@ -154,6 +138,13 @@
           </script>
         </div>
       </div>
+
+      <div id="text-6" class="widget-sidebar frontier-widget widget_text">
+        <div class="textwidget">
+          <a href="bills.php"><img src="images/Untitled.png"></a>
+        </div>
+      </div>
+
       <div id="text-4" class="widget-sidebar frontier-widget widget_text">
         <h4 class="widget-title">डाउनलोड्स</h4>
         <div class="textwidget">
@@ -165,6 +156,22 @@
             </a><br>
           <? }?>
         </div>
-      </div>    
+      </div>
+
+      <div id="notice_board_widget-2" class="widget-sidebar frontier-widget widget_notice_board_widget">
+      <h4 class="widget-title">सूचना </h4>
+      <div class="msnb_notice scroll-up">
+        <ul class="notice-list">
+          <?php
+          $news=$groups->getByParentId(NEWS);
+          while($newsGet=$conn->fetchArray($news)){?>
+            <li>
+              <a href="<?=$newsGet['urlname'];?>"><?=$newsGet['name'];?></a>
+            </li>
+          <?php }?>
+        </ul>
+      </div>
+    </div>
+
     </div>
   </div>
--- template/sidebar.php
+++ template/sidebar.php
@@ -12,6 +12,17 @@
 				</a>
 			</div>
 		</div>
+		<div id="text-9" class="widget-sidebar frontier-widget widget_text">
+	      <?php
+	      $officer=$groups->getById(OFFICER); $officer=$conn->fetchArray($officer);
+	      ?>
+	      <h4 class="widget-title"><?=$officer['name'];?></h4>
+	      <div class="textwidget">
+	        <a><img src="<?=CMS_GROUPS_DIR.$officer['image'];?>"></a>
+	        <?=$officer['shortcontents'];?>
+	      </div>
+	      <a></a>
+	    </div>
 		<div id="text-3" class="widget-sidebar frontier-widget widget_text">
 			<h4 class="widget-title">उपयोगी लिङ्क्स</h4>
 			<div class="textwidget">
@@ -22,20 +33,6 @@
 						<?=$linksGet['name']?>
 					</a><br>
 				<?php }?>
-			</div>
-		</div>
-		<div id="notice_board_widget-2" class="widget-sidebar frontier-widget widget_notice_board_widget">
-			<h4 class="widget-title">सूचना </h4>
-			<div class="msnb_notice scroll-up">
-				<ul class="notice-list">
-					<?php
-					$news=$groups->getByParentId(NEWS);
-					while($newsGet=$conn->fetchArray($news)){?>
-						<li>
-							<a href="<?=$newsGet['urlname'];?>"><?=$newsGet['name'];?></a>
-						</li>
-					<?php }?>
-				</ul>
 			</div>
 		</div>		
 	</div>
